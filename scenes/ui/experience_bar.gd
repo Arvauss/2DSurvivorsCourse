1ee37d924dfd245ab5ae938f3571e0752fed2126
Luke <dewald0725@gmail.com>
2024-08-07 10:34:41 +0200

    functional exp bar

---
+++ scenes/ui/experience_bar.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+
+@export var experience_manager : Node
+@onready var progress_bar = $MarginContainer/ProgressBar
+
+func _ready():
+	progress_bar.value = 0
+	experience_manager.experience_updated.connect(on_experience_update)
+	
+	
+func on_experience_update(current_experience : float, target_experience: float):
+	if current_experience == 0:
+		progress_bar.value = 0
+	var percent = current_experience / target_experience
+	progress_bar.value = percent
