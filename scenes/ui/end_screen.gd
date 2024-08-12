23dfc0b37531ebf51427b0a20f59950ecf349b83
Luke <dewald0725@gmail.com>
2024-08-12 15:01:38 +0200

    end of game victory/defeat screens || player healthbar

---
+++ scenes/ui/end_screen.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var title_label = $%TitleLabel
+@onready var description_label = $%DescriptionLabel
+
+func _ready():
+	get_tree().paused = true
+	
+	$%RestartButton.pressed.connect(on_restart_button_pressed)
+	$%QuitButton.pressed.connect(on_quit_button_pressed)
+	
+	
+func set_defeat():
+	title_label.text = "DEFEAT"
+	description_label.text = "You Lost! :[	"
+	
+	
+func on_restart_button_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+		
+	
+func on_quit_button_pressed():
+	get_tree().quit()
